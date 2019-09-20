9af1e296f07b6b61fe3e317e08d4d9c696b3efe8
HITESH SANTWANI <hiteshsantwani35@gmail.com>
2019-09-20 14:29:31 -0400

    Update FrontAppApiCalls.js

--- FrontAppApiCalls.js
+++ FrontAppApiCalls.js
@@ -1,17 +1,75 @@
 var conversation;
 
-function Notes() {
-    Front.fetchNotes(conversation);
-  }
+function unassign() {
+  Front.unassign(conversation);
+}
+
+function toggleArchive() {
+  Front.toggleArchive(conversation);
+}
+
+function toggleTrashed() {
+  Front.toggleTrashed(conversation);
+}
+
+function reply() {
+  Front.reply({
+    body: 'Template reply',
+    subject: 'Template subject',
+  }, false, conversation);
+}
+
+function alertDialog() {
+  Front.dialog('alert', {
+    title: 'I\'m an alert dialog',
+    message: 'You are now alerted',
+  }, function () {
+    console.log('Alert closed');
+  });
+}
+
+function confirmDialog() {
+  Front.dialog('confirm', {
+    title: 'I\'m a confirm dialog',
+    message: 'Do you confirm',
+    okTitle: 'OK Button',
+    cancelTitle: 'Cancel Button'
+  }, function (confirmed) {
+    if (confirmed)
+      console.log('User confirmed');
+    else
+      console.log('User cancelled');
+  });
+}
+
+function promptDialog() {
+  Front.dialog('prompt', {
+    title: 'I\'m a prompt dialog',
+    message: 'Please enter something'
+  }, function (data) {
+    if (data)
+      console.log('User input :', data);
+    else
+      console.log('User cancelled');
+  });
+}
+
+function fetchTeammates() {
+  Front.fetchAllowedTeammates(function (teammates) {
+    if (!teammates)
+      return;
+
+    console.log(teammates);
+  });
+}
 
 function fetchInboxes() {
-  
-  Front.on('conversation', function (data) {
-    
-    console.log(data.msg_);
+  Front.fetchInboxes(function (inboxes) {
+    if (!inboxes)
+      return;
 
-});
-
+    console.log(inboxes);
+  });
 }
 
 Front.on('conversation', function (data) {
@@ -21,4 +79,3 @@ Front.on('conversation', function (data) {
   console.log('OtherMessages', data.otherMessages);
   conversation = data.conversation;
 });
-
