18642f0961a2ca241afc9675f4e14fd6f8d5274f
HITESH SANTWANI <hiteshsantwani35@gmail.com>
2019-09-20 14:18:14 -0400

    Update FrontAppApiCalls.js

--- FrontAppApiCalls.js
+++ FrontAppApiCalls.js
@@ -14,3 +14,11 @@ function fetchInboxes() {
 
 }
 
+Front.on('conversation', function (data) {
+  console.log('Conversation', data.conversation);
+  console.log('Contact', data.contact);
+  console.log('Message', data.message);
+  console.log('OtherMessages', data.otherMessages);
+  conversation = data.conversation;
+});
+
