0c4204626ef770d65468da43a287a2316722285d
HITESH SANTWANI <hiteshsantwani35@gmail.com>
2019-12-18 19:46:31 -0500

    Update File_Message.js

--- File_Message.js
+++ File_Message.js
@@ -48,17 +48,13 @@ function fileMessage() {
         });
         PopulateData("Messages", document.getElementById("pfId").innerHTML)
 }
-
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
   }
-  
   console.log(uuidv4());
-
-
   function ShowHideDetails() {
 
     $("#FileMessageInput").toggle();
